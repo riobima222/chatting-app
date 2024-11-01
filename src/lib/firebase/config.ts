1733eac718b598c0b9f250c15cb2807dd89e584e
patriobimasuci354 <patriobimasuci354@gmail.com>
2024-11-01 07:44:51 +0000

    main / fix environment variable

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5lGvftAeo9weHztjIaRf0aQDMi4O66xw",
-  authDomain: "web990-d571d.firebaseapp.com",
-  projectId: "web990-d571d",
-  storageBucket: "web990-d571d.appspot.com",
-  messagingSenderId: "220591760665",
-  appId: "1:220591760665:web:115eb926f5c6b85abf9188",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
